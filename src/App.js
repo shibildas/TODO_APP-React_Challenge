944756a8f3b109f05fe31ce0a67512d1878d8ae1
shibildas <shibildas@gmail.com>
2023-03-01 11:00:08 +0530

    master blank

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Counter from "./Counter";
-import { useState } from "react";
 
 
 function App() {
-  const {state,setState} = useState(false)
+
   return (
     <div className="App">
 
-    <h1 onClick={()=>setState(!state)}> show/hide</h1>
+    <h1> show/hide</h1>
 
-    {state ? <Counter /> : null}
     </div>
   );
 }
