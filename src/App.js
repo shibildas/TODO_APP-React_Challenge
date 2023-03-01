8776cfdeeb780f12b9cb744bd0ca14813b35da5b
shibildas <shibildas@gmail.com>
2023-03-01 14:20:23 +0530

    project video final with cancel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 function App() {
-
+  const [toDos,setToDos]=useState([])
+  const [toDo, setToDo] = useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +14,45 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=> setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus" onClick={()=>setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])}></i>
       </div>
-      <div className="todos">
-        <div className="todo">
+     
+     <div className="todos">
+     { 
+     toDos.map((value)=>{
+     
+        return(<div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{
+          console.log(e.target.value);
+          console.log(value);
+          setToDos(toDos.filter(value2=>{
+            if (value2.id === value.id){
+              value2.status = e.target.checked
+            }
+            return value2 
+          }))
+        }} value={value.status} type="checkbox" name="" id="" />
+            <p>{value.text}</p>
+            
           </div>
           <div className="right">
-            <i className="fas fa-times"></i>
+            <i className="fas fa-times" onClick={(e)=>{
+              setToDos(toDos.filter((value2) =>{
+                if(value2.id === value.id){
+                  value2 = null
+                }return value2
+              }))
+
+            }}></i>
           </div>
-        </div>
+        </div>)})}
+        {toDos.map((value)=>{
+          if (value.status){
+            return <h1>{value.text}</h1>
+          }return null
+        })}
       </div>
     </div>
   );
